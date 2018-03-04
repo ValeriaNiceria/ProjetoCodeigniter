b9f59acbfc7bf6eba4fc2c9f86a3fae316ce8987
ValeriaNiceria <valerianiceria@gmail.com>
2018-03-04 20:45:59 -0300

    Estruturação das Views HTML no CodeIgniter

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,9 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->view('includes/html_header');
+		$this->load->view('includes/menu');
 		$this->load->view('dashboard');
+		$this->load->view('/includes/html_footer');
 	}
 }
---
+++ application/views/includes/html_header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?= base_url('assets/css/dashboard.css') ?>" rel="stylesheet">
+  </head>
+
+  <body>
